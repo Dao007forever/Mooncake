231e5a00b044b1047856204fafc0ea7f72f7f98c
Dao Le <daole@inferact.ai>
2026-04-18 23:29:13 -0700

    Try

--- benchmarks/distributed_store_benchmark/counting_nic.sh
+++ benchmarks/distributed_store_benchmark/counting_nic.sh
@@ -1,7 +1,15 @@
-for d in mlx5_3 mlx5_4; do
-  echo -n "$d rcv_rate: "
-  a=$(cat /sys/class/infiniband/$d/ports/1/counters/port_rcv_data)
-  sleep 2
-  b=$(cat /sys/class/infiniband/$d/ports/1/counters/port_rcv_data)
-  python3 -c "print(f'{($b-$a)*4/2/1e9:.1f} GB/s')"
+DEVS=(mlx5_0 mlx5_1)
+WINDOW=${1:-5}
+
+declare -A BEFORE
+for d in "${DEVS[@]}"; do
+  BEFORE[$d]=$(cat /sys/class/infiniband/$d/ports/1/counters/port_rcv_data)
+done
+
+sleep "$WINDOW"
+
+for d in "${DEVS[@]}"; do
+  after=$(cat /sys/class/infiniband/$d/ports/1/counters/port_rcv_data)
+  before=${BEFORE[$d]}
+  python3 -c "print(f'$d rcv_rate: {($after-$before)*4/$WINDOW/1e9:.1f} GB/s')"
 done
